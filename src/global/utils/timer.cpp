499c2fdf8bc5418645c0919b0a479eb8c91b5060
jmahlmann <jens.mahlmann@gmail.com>
2025-03-13 22:59:23 -0400

    Ongoing

--- src/global/utils/timer.cpp
+++ src/global/utils/timer.cpp
@@ -44,7 +44,7 @@ namespace timer {
                  mpi::get_type<duration_t>(),
                  MPI_ROOT_RANK,
                  MPI_COMM_WORLD);
-    printf(all_timers[name]);
+    printf("%f", all_timers[name]);
     }
     // accumulate nparts and ncells from MPI blocks
     auto all_nparts = std::vector<npart_t>(size, 0);
