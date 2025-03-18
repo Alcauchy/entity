3695013f7be8bd4baa70b5af775b023f5b69a8a9
jmahlmann <jens.mahlmann@gmail.com>
2025-03-18 00:37:02 -0400

    Ongoing

--- src/framework/containers/particles.cpp
+++ src/framework/containers/particles.cpp
@@ -75,39 +75,64 @@ namespace ntt {
     -> std::pair<std::vector<npart_t>, array_t<npart_t*>> {
     auto              this_tag = tag;
     const auto        num_tags = ntags();
-    array_t<npart_t> npptag { "nparts_per_tag", ntags() };
+    array_t<npart_t*> npptag { "nparts_per_tag", ntags() };
 
-    // count # of particles per each tag
-    auto npptag_scat = Kokkos::Experimental::create_scatter_view(npptag);
-    Kokkos::parallel_for(
-      "NpartPerTag",
-      rangeActiveParticles(),
-      Lambda(index_t p) {
-        auto npptag_acc = npptag_scat.access();
-        if (this_tag(p) < 0 || this_tag(p) >= num_tags) {
-          raise::KernelError(HERE, "Invalid tag value");
-        }
-        npptag_acc(this_tag(p)) += 1;
-      });
-    Kokkos::Experimental::contribute(npptag, npptag_scat);
+    // // count # of particles per each tag
+    // auto npptag_scat = Kokkos::Experimental::create_scatter_view(npptag);
+    // Kokkos::parallel_for(
+    //   "NpartPerTag",
+    //   rangeActiveParticles(),
+    //   Lambda(index_t p) {
+    //     auto npptag_acc = npptag_scat.access();
+    //     if (this_tag(p) < 0 || this_tag(p) >= num_tags) {
+    //       raise::KernelError(HERE, "Invalid tag value");
+    //     }
+    //     npptag_acc(this_tag(p)) += 1;
+    //   });
+    // Kokkos::Experimental::contribute(npptag, npptag_scat);
 
-    // copy the count to a vector on the host
-    auto npptag_h = Kokkos::create_mirror_view(npptag);
-    Kokkos::deep_copy(npptag_h, npptag);
-    std::vector<npart_t> npptag_vec(num_tags);
-    for (auto t { 0u }; t < num_tags; ++t) {
-      npptag_vec[t] = npptag_h(t);
-    }
+    // // copy the count to a vector on the host
+    // auto npptag_h = Kokkos::create_mirror_view(npptag);
+    // Kokkos::deep_copy(npptag_h, npptag);
+    // std::vector<npart_t> npptag_vec(num_tags);
+    // for (auto t { 0u }; t < num_tags; ++t) {
+    //   npptag_vec[t] = npptag_h(t);
+    // }
 
-    // count the offsets on the host and copy to device
-    array_t<npart_t*> tag_offsets("tag_offsets", num_tags - 3);
-    auto              tag_offsets_h = Kokkos::create_mirror_view(tag_offsets);
+    // // count the offsets on the host and copy to device
+    // array_t<npart_t*> tag_offsets("tag_offsets", num_tags - 3);
+    // auto              tag_offsets_h = Kokkos::create_mirror_view(tag_offsets);
 
-    tag_offsets_h(0) = npptag_vec[2]; // offset for tag = 3
-    for (auto t { 1u }; t < num_tags - 3; ++t) {
-      tag_offsets_h(t) = npptag_vec[t + 2] + tag_offsets_h(t - 1);
-    }
-    Kokkos::deep_copy(tag_offsets, tag_offsets_h);
+    // tag_offsets_h(0) = npptag_vec[2]; // offset for tag = 3
+    // for (auto t { 1u }; t < num_tags - 3; ++t) {
+    //   tag_offsets_h(t) = npptag_vec[t + 2] + tag_offsets_h(t - 1);
+    // }
+    // Kokkos::deep_copy(tag_offsets, tag_offsets_h);
+
+    std::vector<npart_t> npptag_vec;
+     for (npart_t t { 0 }; t < num_tags; ++t) {
+      npart_t npart_tag_i = 0;
+       Kokkos::parallel_reduce(
+         "NpartPerTag",
+       npart(),
+       Lambda(index_t p, npart_t& loc_npart_tag) {
+         if (this_tag(p) == t) {
+           loc_npart_tag++;
+         }
+       }, npart_tag_i);
+       npptag_vec.push_back(npart_tag_i);
+     }
+ 
+ 
+     // count the offsets on the host and copy to device
+     array_t<npart_t*> tag_offsets("tag_offsets", num_tags - 3);
+     auto tag_offsets_h = Kokkos::create_mirror_view(tag_offsets);
+ 
+     tag_offsets_h(0) = npptag_vec[2]; // offset for tag = 3
+     for (auto t { 1u }; t < num_tags - 3; ++t) {
+       tag_offsets_h(t) = npptag_vec[t + 2] + tag_offsets_h(t - 1);
+     }
+     Kokkos::deep_copy(tag_offsets, tag_offsets_h);
 
     return { npptag_vec, tag_offsets };
   }
