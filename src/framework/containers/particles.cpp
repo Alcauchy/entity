d3a37e8495c5ca0c6446014892f516f564159f48
jmahlmann <jens.mahlmann@gmail.com>
2025-03-17 23:40:28 -0400

    Ongoing

--- src/framework/containers/particles.cpp
+++ src/framework/containers/particles.cpp
@@ -84,12 +84,12 @@ namespace ntt {
       rangeActiveParticles(),
       Lambda(index_t p) {
         auto npptag_acc = npptag_scat.access();
-        if (this_tag(p) < 0 || this_tag(p) >= num_tags) {
-          raise::KernelError(HERE, "Invalid tag value");
-        }
-        npptag_acc(this_tag(p)) += 1;
+        // if (this_tag(p) < 0 || this_tag(p) >= num_tags) {
+        //   raise::KernelError(HERE, "Invalid tag value");
+        // }
+        // npptag_acc(this_tag(p)) += 1;
       });
-    // Kokkos::Experimental::contribute(npptag, npptag_scat);
+    Kokkos::Experimental::contribute(npptag, npptag_scat);
 
     // // copy the count to a vector on the host
     auto npptag_h = Kokkos::create_mirror_view(npptag);
